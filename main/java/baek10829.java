b903d2a6204baa66c61639d66d37b6266bf63cca
naocya@naver.com <naocya@naver.com>
2021-11-07 20:13:00 +0900

    baek10829

---
+++ main/java/baek10829.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class baek10829 {
+    // baek 10829 : https://www.acmicpc.net/problem/10829
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long inputNum = Long.parseLong(br.readLine());
+        ArrayList<Long> numArrList = new ArrayList<>();
+
+        for (int i = 0; 0 < inputNum; i++) {
+            long num = inputNum % 2;
+            inputNum /= 2;
+            numArrList.add(num);
+        }
+
+        Collections.reverse(numArrList);
+
+        for (long num:numArrList) {
+            System.out.printf("%d", num);
+        }
+    }
+}
+
