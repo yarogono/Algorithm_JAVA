88a04e723496484f5c9ef5daadc3b488ecaf1bb2
naocya@naver.com <naocya@naver.com>
2021-11-09 01:23:41 +0900

    문자열 다루기 기본

---
+++ main/java/programmers/hanghae/programmers17.java
@@ -0,0 +1,23 @@
+package programmers.hanghae;
+
+public class programmers17 {
+    // 프로그래머스 문자열 다루기 기본 => https://programmers.co.kr/learn/courses/30/lessons/12918
+    public boolean solution(String s) {
+        boolean answer = true;
+        System.out.println(s.length());
+
+        if(s.length() != 4) {
+            answer = false;
+            return answer;
+        }
+
+        for(int i = 0; i < s.length(); i++) {
+            char tempChar = s.charAt(i);
+            if(Character.getNumericValue(tempChar) >= 10) {
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
+}
