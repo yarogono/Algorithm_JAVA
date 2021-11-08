7a986ac5ef732f9e05b22b22581cdcb3517ef525
naocya@naver.com <naocya@naver.com>
2021-11-08 16:33:40 +0900

    핸드폰 번호 가리기

---
+++ main/java/programmers/hanghae/programmers09.java
@@ -0,0 +1,20 @@
+package programmers.hanghae;
+
+public class programmers09 {
+    // 프로그래머스 핸드폰 번호 가리기 => https://programmers.co.kr/learn/courses/30/lessons/12948
+    public String solution(String phone_number) {
+        String answer = "";
+
+        String backStr = phone_number.substring(phone_number.length()-4, phone_number.length());
+        String frondStr = phone_number.substring(0, phone_number.length() - 4);
+
+        StringBuffer sb = new StringBuffer();
+
+        for(int i = 0; i < frondStr.length(); i++) {
+            sb.append("*");
+        }
+
+        answer = sb + backStr;
+        return answer;
+    }
+}
