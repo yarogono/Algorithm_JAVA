673d9aee1a945af659f7668185b2da4506922b53
naocya@naver.com <naocya@naver.com>
2021-11-08 10:37:30 +0900

    문자열을 정수로 바꾸기

---
+++ main/java/programmers/level1/programmers05.java
@@ -0,0 +1,10 @@
+package programmers.level1;
+
+public class programmers05 {
+    // 프로그래머스 문자열을 정수로 바꾸기 => https://programmers.co.kr/learn/courses/30/lessons/12925
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
