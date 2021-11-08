70d674391e328e315919b87bb13f0b8d9d159041
naocya@naver.com <naocya@naver.com>
2021-11-08 10:55:38 +0900

    짝수와 홀수

---
+++ main/java/programmers/level1/programmers02.java
@@ -0,0 +1,14 @@
+package programmers.level1;
+
+public class programmers02 {
+    // 프로그래머스 짝수와 홀수 => https://programmers.co.kr/learn/courses/30/lessons/12937
+    public String solution(int num) {
+        String answer = "";
+        if (num % 2 == 0 || num == 0) {
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
