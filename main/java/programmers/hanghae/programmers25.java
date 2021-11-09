68ad663bf64645bd682267ebd5153f11b05deae0
naocya@naver.com <naocya@naver.com>
2021-11-09 14:11:02 +0900

    정수 제곱근 판별 만들기 수정

--- main/java/programmers/hanghae/programmers25.java
+++ main/java/programmers/hanghae/programmers25.java
@@ -4,14 +4,13 @@ public class programmers25 {
     // 프로그래머스 정수 제곱근 판별 만들기 => https://programmers.co.kr/learn/courses/30/lessons/12934
     public long solution(long n) {
         long answer = 0;
-        double d = Math.sqrt(n);
+        double squareRoot = Math.sqrt(n);   // squareRoot => 제곱근
 
-        if(d - (int)d > 0) {
+        if(squareRoot - (int)squareRoot > 0) {
             answer = - 1;
-            System.out.println(answer);
             return answer;
         }
-        answer = (long)(Math.pow(d + 1, 2));
+        answer = (long)(Math.pow(squareRoot + 1, 2));
 
         return answer;
     }
