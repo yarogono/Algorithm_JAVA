c406545e7f1b1109289ef6b52ef672348ad6eb78
naocya@naver.com <naocya@naver.com>
2021-11-10 16:47:38 +0900

    로또의 최고 순위와 최저 순위 수정

--- main/java/programmers/hanghae/programmers33.java
+++ main/java/programmers/hanghae/programmers33.java
@@ -1,7 +1,5 @@
 package programmers.hanghae;
 
-import java.util.Arrays;
-
 public class programmers33 {
     // 프로그래머스 로또의 최고 순위와 최저 순위 => https://programmers.co.kr/learn/courses/30/lessons/77484
     public int[] solution(int[] lottos, int[] win_nums) {
