dd09cf40403950f372cabe7a12543b76c3b246b0
naocya@naver.com <naocya@naver.com>
2021-11-11 21:07:41 +0900

    신규 아이디 추천

---
+++ main/java/programmers/hanghae/programmers40.java
@@ -0,0 +1,35 @@
+package programmers.hanghae;
+
+public class programmers40 {
+    // 프로그래머스 신규 아이디 추천 => https://programmers.co.kr/learn/courses/30/lessons/72410﻿
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase(); // 1단계
+
+        answer = answer.replaceAll("[^-_.a-z0-9]", ""); // 2단계
+        answer = answer.replaceAll("[.]{2,}", "."); // 3단계
+        answer = answer.replaceAll("^[.]|[.]$", "");    // 4단계
+
+        if (answer.equals("")) {    // 5단계
+            answer += "a";
+        }
+
+        if (answer.length() >= 16) {     // 6단계
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$","");
+        }
+
+        if (answer.length() <= 2) {  // 7단계
+            while (answer.length() < 3) {
+                answer += answer.charAt(answer.length()-1);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        programmers40 test = new programmers40();
+        String new_id = "...!@BaT#*..y.abcdefghijklm";
+        test.solution(new_id);
+    }
+}
