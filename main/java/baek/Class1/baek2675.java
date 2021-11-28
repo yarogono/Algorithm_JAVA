4dc51bf78ef2f673eb11e663ecb9a48b94dbdc25
naocya@naver.com <naocya@naver.com>
2021-11-28 10:22:46 +0900

    baek2675

---
+++ main/java/baek/Class1/baek2675.java
@@ -0,0 +1,32 @@
+package baek.Class1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class baek2675 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        List<String> resultList = new ArrayList<>();
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < t; i++) {
+            int r = sc.nextInt();
+            String tempStr = sc.nextLine();
+            for(int j = 0; j < tempStr.length(); j++) {
+                for(int k = 0; k < r; k++) {
+                    sb.append(tempStr.charAt(j));
+                }
+            }
+
+            resultList.add(sb.toString().trim());
+            sb.delete(0, sb.length());
+        }
+
+
+        for (String tempStr : resultList) {
+            System.out.println(tempStr);
+        }
+    }
+}
