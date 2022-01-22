d696f83a2d56b84fe470ac42e633c6139ec3df4e
naocya@naver.com <naocya@naver.com>
2022-01-22 16:09:53 +0900

    baek11654

---
+++ main/java/baek/Class1/baek11654.java
@@ -0,0 +1,12 @@
+package baek.Class1;
+
+import java.util.Scanner;
+
+public class baek11654 {
+    // baek 11654 : https://www.acmicpc.net/problem/11654
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.next().charAt(0);
+        System.out.println(x);
+    }
+}
