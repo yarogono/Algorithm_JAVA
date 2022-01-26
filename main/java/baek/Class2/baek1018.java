2f6fd0f192a9702c50cfad21175a03f3e6c94375
naocya@naver.com <naocya@naver.com>
2022-01-26 14:06:20 +0900

    baek1018 풀이 추가

--- main/java/baek/Class2/baek1018.java
+++ main/java/baek/Class2/baek1018.java
@@ -1,39 +1,45 @@
 package baek.Class2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class baek1018 {
     // baek 1018 : https://www.acmicpc.net/problem/1018
-
+    // 풀이 참고한 블로그 => https://st-lab.tistory.com/101
     public static  boolean[][] arr;
     public static int min = 64;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 체스판 세로 줄의 갯수
         int n = sc.nextInt();
+        // 체스판 가로 줄의 갯수
         int m = sc.nextInt();
 
+        // 체스판 칸이 검은색인지 흰색인지 boolean 배열로 표시
         arr = new boolean[n][m];
 
+        // 체스판 각 칸이 무엇으로 칠해져 있는지 입력
+        // - W 일때는 true
+        // - B일 때는 false
         for(int i = 0; i < n; i++) {
+            // 세로 줄에 해당하는 각 라인 입력
             String chessLine = sc.next();
-
+            // 각 라인의 칸 W인지 B 인지 확인
             for(int j = 0; j < m; j++) {
                 if (chessLine.charAt(j) == 'W') {
-                    arr[i][j] = true;		// W일 때는 true
+                    arr[i][j] = true;
                 } else {
-                    arr[i][j] = false;		// B일 때는 false
+                    arr[i][j] = false;
                 }
             }
         }
 
-
+        // 경우의수는 (가로 칸 개수 - 7) x (세로 칸 개수 - 7)
         int N_row = n - 7;
         int M_col = m - 7;
 
+        // 8 X 8 크기의 배열을 하나씩 검사
+        // - 첫 칸의 색이 하얀색일 경우와 검은색일 경우의 수를 비교하여 칠한 개수 최솟값 구하기
         for (int i = 0; i < N_row; i++) {
             for (int j = 0; j < M_col; j++) {
                 find(i, j);
@@ -42,12 +48,14 @@ public class baek1018 {
         System.out.println(min);
     }
 
+    // 다시 칠해야 하는 정사각형 개수 최솟값 찾는 함수
     private static void find(int x, int y) {
         int end_x = x + 8;
         int end_y = y + 8;
         int count = 0;
 
-        boolean TF = arr[x][y];	// 첫 번째 칸의 색
+        // 첫 번째 칸의 색
+        boolean TF = arr[x][y];
 
         for (int i = x; i < end_x; i++) {
             for (int j = y; j < end_y; j++) {
