70805b77eaf16f9311b0fddb023d457b569441b5
naocya@naver.com <naocya@naver.com>
2022-04-03 13:56:16 +0900

    백준 10926문제

---
+++ main/java/baek/baek10926.java
@@ -0,0 +1,14 @@
+package baek;
+
+import java.util.Scanner;
+
+public class baek10926 {
+    // baek 10926 : https://www.acmicpc.net/problem/10926
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputStr = sc.nextLine();
+
+        String result = inputStr + "??!";
+        System.out.println(result);
+    }
+}
