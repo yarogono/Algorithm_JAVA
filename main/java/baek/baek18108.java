cf0c2a3641477e53acc2135ae22fcd7fa6e2a32b
naocya@naver.com <naocya@naver.com>
2022-04-03 14:05:50 +0900

    백준18108

---
+++ main/java/baek/baek18108.java
@@ -0,0 +1,15 @@
+package baek;
+
+import java.util.Scanner;
+
+public class baek18108 {
+    // baek 18108 : https://www.acmicpc.net/problem/18108
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inputNum = sc.nextInt();
+
+        int result = inputNum - 543;
+
+        System.out.println(result);
+    }
+}
